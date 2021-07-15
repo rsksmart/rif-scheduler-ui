89f04a7a06e915c092082f0b32dfaefc386d8e3d
Christian Escalante <chescalante.ar@gmail.com>
2021-07-15 12:51:58 -0300

    added example contract for testnet and minor changes

--- src/home/Contracts.tsx
+++ src/home/Contracts.tsx
@@ -1,4 +1,3 @@
-
 import {
   Avatar,
   Card,
@@ -7,60 +6,49 @@ import {
   Typography,
   makeStyles,
   colors,
-  Link
-} from '@material-ui/core';
-import ContractsIcon from "@material-ui/icons/Extension";
-import useConnector from '../connect/useConnector';
-import { Link as RouterLink } from "react-router-dom"
-import useContracts from '../contracts/useContracts';
+  Link,
+} from "@material-ui/core";
+import ContractsIcon from "@material-ui/icons/OfflineBolt";
+import useConnector from "../connect/useConnector";
+import { Link as RouterLink } from "react-router-dom";
+import useContracts from "../contracts/useContracts";
 
 const useStyles = makeStyles(() => ({
   root: {
-    height: '100%',
+    height: "100%",
   },
   avatar: {
-    backgroundColor: colors.blue[600],
+    backgroundColor: colors.blueGrey[600],
     height: 56,
-    width: 56
-  }
+    width: 56,
+  },
 }));
 
 const Contracts = ({ className, ...rest }: any) => {
   const classes = useStyles();
 
-  const connectedToNetwork = useConnector(state => state.network)
+  const connectedToNetwork = useConnector((state) => state.network);
 
   const contracts = useContracts((state) => state.contracts);
 
-  const networkContracts = Object.entries(contracts)
-    .filter(([id, contract])=> contract.network === connectedToNetwork)
+  const networkContracts = Object.entries(contracts).filter(
+    ([id, contract]) => contract.network === connectedToNetwork
+  );
 
   return (
-    <Card
-      className={[classes.root, className].join(" ")}
-      {...rest}
-    >
+    <Card className={[classes.root, className].join(" ")} {...rest}>
       <CardContent>
-        <Grid
-          container
-          justify="space-between"
-          spacing={3}
-        >
-          <Grid item style={{ flex:1 }}>
-            <Typography
-              color="textSecondary"
-              gutterBottom
-              variant="h6"
-            >
+        <Grid container justify="space-between" spacing={3}>
+          <Grid item style={{ flex: 1 }}>
+            <Typography color="textSecondary" gutterBottom variant="h6">
               REGISTERED CONTRACTS
             </Typography>
-            <Typography
-              color="textPrimary"
-              variant="h3"
-            >
+            <Typography color="textPrimary" variant="h3">
               {networkContracts.length}
             </Typography>
-            <Link component={RouterLink} to="/contracts">Go to contracts &rsaquo;</Link>
+            <Link component={RouterLink} color="textSecondary" to="/contracts">
+              Go to contracts &rsaquo;
+            </Link>
           </Grid>
           <Grid item>
             <Avatar className={classes.avatar}>
--- src/home/Home.tsx
+++ src/home/Home.tsx
@@ -1,32 +1,34 @@
-import Layout from "../shared/Layout"
-import Contracts from "./Contracts"
-import Executions from "./Executions"
-import Network from "./Network"
-import Schedule from "./Schedule"
-import ScheduleHistory from "./ScheduleHistory"
-import TasksProgress from "./TasksProgress"
+import Layout from "../shared/Layout";
+import Contracts from "./Contracts";
+import Executions from "./Executions";
+import Network from "./Network";
+import Schedule from "./Schedule";
+import ScheduleHistory from "./ScheduleHistory";
+import TasksProgress from "./TasksProgress";
 
 const Home = () => {
-    return (
-        <Layout>
-            <div style={{
-                display: "grid",
-                gridTemplateColumns: "repeat(auto-fill, minmax(300px, 2fr))",
-                gridAutoRows: "160px",
-                gridGap: "5px",
-                justifyContent: "space-between",
-                width: "100%",
-                maxWidth: 800,
-            }}>
-                <TasksProgress />
-                <Network />
-                <Executions />
-                <Contracts />
-                <Schedule />
-                <ScheduleHistory />
-            </div>
-        </Layout>
-    )
-}
+  return (
+    <Layout>
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(auto-fill, minmax(300px, 2fr))",
+          gridAutoRows: "180px",
+          gridGap: "5px",
+          justifyContent: "space-between",
+          width: "100%",
+          maxWidth: 800,
+        }}
+      >
+        <TasksProgress />
+        <Network />
+        <Executions />
+        <Contracts />
+        <Schedule />
+        <ScheduleHistory />
+      </div>
+    </Layout>
+  );
+};
 
-export default Home
+export default Home;
