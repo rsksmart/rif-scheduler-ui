2fbb61d618efea74d90574468a1c07a3f5c5df69
Christian Escalante <chescalante.ar@gmail.com>
2021-07-20 13:17:20 -0300

    added example contract for testnet and minor changes (#17)

--- src/home/Executions.tsx
+++ src/home/Executions.tsx
@@ -1,4 +1,3 @@
-
 import {
   Avatar,
   Card,
@@ -7,73 +6,66 @@ import {
   Typography,
   makeStyles,
   colors,
-  Link
-} from '@material-ui/core';
-import StoreIcon from '@material-ui/icons/Store';
-import { BigNumber } from 'ethers';
-import useConnector from '../connect/useConnector';
-import { formatBigNumber } from '../shared/formatters';
-import useProviders from '../store/useProviders';
-import { Link as RouterLink } from "react-router-dom"
+  Link,
+} from "@material-ui/core";
+import StoreIcon from "@material-ui/icons/Store";
+import { BigNumber } from "ethers";
+import useConnector from "../connect/useConnector";
+import { formatBigNumber } from "../shared/formatters";
+import useProviders from "../store/useProviders";
+import { Link as RouterLink } from "react-router-dom";
 
 const useStyles = makeStyles(() => ({
   root: {
-    height: '100%',
+    height: "100%",
   },
   avatar: {
-    backgroundColor: colors.blue[600],
+    backgroundColor: colors.blueGrey[600],
     height: 56,
-    width: 56
-  }
+    width: 56,
+  },
 }));
 
-const BIG_ZERO = BigNumber.from(0)
+const BIG_ZERO = BigNumber.from(0);
 
 const Executions = ({ className, ...rest }: any) => {
   const classes = useStyles();
 
-  const connectedToNetwork = useConnector(state => state.network)
+  const connectedToNetwork = useConnector((state) => state.network);
 
-  const providers = useProviders((state) => state.providers)
+  const providers = useProviders((state) => state.providers);
 
-  const networkProviders = Object.entries(providers)
-    .filter(([id, provider])=> provider.network === connectedToNetwork)
+  const networkProviders = Object.entries(providers).filter(
+    ([id, provider]) => provider.network === connectedToNetwork
+  );
 
-  const executionsLeft = networkProviders.reduce((accumulated, [id, provider])=>{
+  const executionsLeft = networkProviders.reduce(
+    (accumulated, [id, provider]) => {
+      const providerExecutionsLeft = provider.plans.reduce(
+        (accumulatedPlans, currentPlan) =>
+          accumulatedPlans.add(currentPlan.remainingExecutions ?? BIG_ZERO),
+        BIG_ZERO
+      );
 
-    const providerExecutionsLeft = provider.plans
-      .reduce((accumulatedPlans, currentPlan) => accumulatedPlans.add(currentPlan.remainingExecutions ?? BIG_ZERO), BIG_ZERO)
-
-    return accumulated.add(providerExecutionsLeft)
-
-  }, BIG_ZERO)
+      return accumulated.add(providerExecutionsLeft);
+    },
+    BIG_ZERO
+  );
 
   return (
-    <Card
-      className={[classes.root, className].join(" ")}
-      {...rest}
-    >
+    <Card className={[classes.root, className].join(" ")} {...rest}>
       <CardContent>
-        <Grid
-          container
-          justify="space-between"
-          spacing={3}
-        >
-          <Grid item style={{ flex:1 }}>
-            <Typography
-              color="textSecondary"
-              gutterBottom
-              variant="h6"
-            >
+        <Grid container justify="space-between" spacing={3}>
+          <Grid item style={{ flex: 1 }}>
+            <Typography color="textSecondary" gutterBottom variant="h6">
               REMAINING EXECUTIONS
             </Typography>
-            <Typography
-              color="textPrimary"
-              variant="h3"
-            >
+            <Typography color="textPrimary" variant="h3">
               {formatBigNumber(executionsLeft, 0)}
             </Typography>
-            <Link component={RouterLink} to="/store">Go to store &rsaquo;</Link>
+            <Link component={RouterLink} color="textSecondary" to="/store">
+              Go to store &rsaquo;
+            </Link>
           </Grid>
           <Grid item>
             <Avatar className={classes.avatar}>
--- src/home/ScheduleHistory.tsx
+++ src/home/ScheduleHistory.tsx
@@ -1,4 +1,3 @@
-
 import {
   Avatar,
   Card,
@@ -7,61 +6,52 @@ import {
   Typography,
   makeStyles,
   colors,
-  Link
-} from '@material-ui/core';
+  Link,
+} from "@material-ui/core";
 import ScheduleIcon from "@material-ui/icons/Timeline";
-import useConnector from '../connect/useConnector';
-import { Link as RouterLink } from "react-router-dom"
-import useSchedule from '../schedule/useSchedule';
-import { ExecutionState } from '@rsksmart/rif-scheduler-sdk';
+import useConnector from "../connect/useConnector";
+import { Link as RouterLink } from "react-router-dom";
+import useSchedule from "../schedule/useSchedule";
+import { ExecutionState } from "@rsksmart/rif-scheduler-sdk";
 
 const useStyles = makeStyles(() => ({
   root: {
-    height: '100%',
+    height: "100%",
   },
   avatar: {
-    backgroundColor: colors.blue[600],
+    backgroundColor: colors.blueGrey[600],
     height: 56,
-    width: 56
-  }
+    width: 56,
+  },
 }));
 
 const ScheduleHistory = ({ className, ...rest }: any) => {
   const classes = useStyles();
 
-  const connectedToNetwork = useConnector(state => state.network)
+  const connectedToNetwork = useConnector((state) => state.network);
 
   const scheduledExecutions = useSchedule((state) => state.scheduleItems);
 
-  const networkScheduledExecutions = Object.entries(scheduledExecutions)
-    .filter(([id, execution])=> execution.network === connectedToNetwork && execution.state !== ExecutionState.Nonexistent)
+  const networkScheduledExecutions = Object.entries(scheduledExecutions).filter(
+    ([id, execution]) =>
+      execution.network === connectedToNetwork &&
+      execution.state !== ExecutionState.Nonexistent
+  );
 
   return (
-    <Card
-      className={[classes.root, className].join(" ")}
-      {...rest}
-    >
+    <Card className={[classes.root, className].join(" ")} {...rest}>
       <CardContent>
-        <Grid
-          container
-          justify="space-between"
-          spacing={3}
-        >
-          <Grid item style={{ flex:1 }}>
-            <Typography
-              color="textSecondary"
-              gutterBottom
-              variant="h6"
-            >
+        <Grid container justify="space-between" spacing={3}>
+          <Grid item style={{ flex: 1 }}>
+            <Typography color="textSecondary" gutterBottom variant="h6">
               EXECUTIONS TOTAL
             </Typography>
-            <Typography
-              color="textPrimary"
-              variant="h3"
-            >
+            <Typography color="textPrimary" variant="h3">
               {networkScheduledExecutions.length}
             </Typography>
-            <Link component={RouterLink} to="/schedule">Go to schedule &rsaquo;</Link>
+            <Link component={RouterLink} color="textSecondary" to="/schedule">
+              Go to schedule &rsaquo;
+            </Link>
           </Grid>
           <Grid item>
             <Avatar className={classes.avatar}>
